12a8b69ac8c709fa3c4fee28b5ae74ef7838e549
Awais Saeed <awais.saeed@forenax.com>
2022-03-08 18:54:04 +0500

    chore: remove unnecessary product apis
    Signed-off-by: Awais Saeed <awais.saeed@forenax.com>

--- examples/redis-cache/src/controllers/product.controller.ts
+++ examples/redis-cache/src/controllers/product.controller.ts
@@ -11,9 +11,7 @@ import {
   get,
   getModelSchemaRef,
   param,
-  patch,
   post,
-  put,
   requestBody,
   response,
 } from '@loopback/rest';
@@ -75,25 +73,6 @@ export class ProductController {
     return this.productRepository.find(filter);
   }
 
-  @patch('/products')
-  @response(200, {
-    description: 'Product PATCH success count',
-    content: {'application/json': {schema: CountSchema}},
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Product, {partial: true}),
-        },
-      },
-    })
-    product: Product,
-    @param.where(Product) where?: Where<Product>,
-  ): Promise<Count> {
-    return this.productRepository.updateAll(product, where);
-  }
-
   @cache(60)
   @get('/products/{id}')
   @response(200, {
@@ -110,35 +89,6 @@ export class ProductController {
     filter?: FilterExcludingWhere<Product>,
   ): Promise<Product> {
     return this.productRepository.findById(id, filter);
-  }
-
-  @patch('/products/{id}')
-  @response(204, {
-    description: 'Product PATCH success',
-  })
-  async updateById(
-    @param.path.number('id') id: number,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Product, {partial: true}),
-        },
-      },
-    })
-    product: Product,
-  ): Promise<void> {
-    await this.productRepository.updateById(id, product);
-  }
-
-  @put('/products/{id}')
-  @response(204, {
-    description: 'Product PUT success',
-  })
-  async replaceById(
-    @param.path.number('id') id: number,
-    @requestBody() product: Product,
-  ): Promise<void> {
-    await this.productRepository.replaceById(id, product);
   }
 
   @del('/products/{id}')
